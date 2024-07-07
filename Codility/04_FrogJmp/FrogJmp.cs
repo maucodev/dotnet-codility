0d0c238f1282ffae0476dab6f817da8ee083834a
maucodev <hola@maucodev.com>
2024-07-07 17:31:13 -0600

    Implementing FrogJmp

---
+++ Codility/04_FrogJmp/FrogJmp.cs
@@ -0,0 +1,11 @@
+﻿// ReSharper disable InconsistentNaming
+
+namespace Codility._04_FrogJmp;
+
+public static class FrogJmp
+{
+    public static int Solution(int X, int Y, int D)
+    {
+        return (int)Math.Ceiling((double)(Y - X) / D);
+    }
+}
---
+++ CodilityUnitTests/04_FrogJmp/FrogJmpUnitTests.cs
@@ -0,0 +1,82 @@
+﻿namespace CodilityUnitTests._04_FrogJmp;
+
+using Codility._04_FrogJmp;
+using FluentAssertions;
+
+public class FrogJmpUnitTests
+{
+    [Fact]
+    public void Solution_SimpleCase_ReturnsCorrectJumps()
+    {
+        // Arrange
+        const int x = 10;
+        const int y = 85;
+        const int d = 30;
+
+        // Act
+        var result = FrogJmp.Solution(x, y, d);
+
+        // Assert
+        result.Should().Be(3);
+    }
+
+    [Fact]
+    public void Solution_ExactJumps_ReturnsCorrectJumps()
+    {
+        // Arrange
+        const int x = 10;
+        const int y = 70;
+        const int d = 30;
+
+        // Act
+        var result = FrogJmp.Solution(x, y, d);
+
+        // Assert
+        result.Should().Be(2);
+    }
+
+    [Fact]
+    public void Solution_NoJumpsNeeded_ReturnsZero()
+    {
+        // Arrange
+        const int x = 10;
+        const int y = 10;
+        const int d = 30;
+
+        // Act
+        var result = FrogJmp.Solution(x, y, d);
+
+        // Assert
+        result.Should().Be(0);
+    }
+
+    [Fact]
+    public void Solution_MinimalCase_ReturnsCorrectJumps()
+    {
+        // Arrange
+        const int x = 1;
+        const int y = 1_000_000_000;
+        const int d = 1;
+
+        // Act
+        var result = FrogJmp.Solution(x, y, d);
+
+        // Assert
+        result.Should().Be(999_999_999);
+    }
+
+    [Fact]
+    public void Solution_LargeJumpDistance_ReturnsCorrectJumps()
+    {
+        // Arrange
+        const int x = 1;
+        const int y = 1_000_000_000;
+        const int d = 1_000_000_000;
+
+        // Act
+        var result = FrogJmp.Solution(x, y, d);
+
+        // Assert
+        result.Should().Be(1);
+    }
+}
