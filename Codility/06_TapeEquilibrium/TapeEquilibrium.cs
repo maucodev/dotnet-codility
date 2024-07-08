b98b5c29f70305cc36fd5dd5c2f88b97a5c21f1e
maucodev <hola@maucodev.com>
2024-07-07 18:54:14 -0600

    Implementing TapeEquilibrium

---
+++ Codility/06_TapeEquilibrium/TapeEquilibrium.cs
@@ -0,0 +1,32 @@
+﻿// ReSharper disable InconsistentNaming
+
+using System;
+using System.Linq;
+
+namespace Codility._06_TapeEquilibrium;
+
+public static class TapeEquilibrium
+{
+    public static int Solution(int[] A)
+    {
+        var totalSum = A.Sum();
+        var leftSum = 0;
+        var rightSum = totalSum;
+        var minDifference = int.MaxValue;
+
+        for (var i = 0; i < A.Length - 1; i++)
+        {
+            leftSum += A[i];
+            rightSum -= A[i];
+
+            var difference = Math.Abs(leftSum - rightSum);
+
+            if (difference < minDifference)
+            {
+                minDifference = difference;
+            }
+        }
+
+        return minDifference;
+    }
+}
---
+++ CodilityUnitTests/06_TapeEquilibrium/TapeEquilibriumUnitTests.cs
@@ -0,0 +1,74 @@
+﻿// ReSharper disable InconsistentNaming
+
+using Codility._06_TapeEquilibrium;
+using FluentAssertions;
+
+namespace CodilityUnitTests._06_TapeEquilibrium;
+
+public class TapeEquilibriumUnitTests
+{
+    [Fact]
+    public void Solution_BasicCase1_ReturnsCorrect()
+    {
+        // Arrange
+        int[] A = [3, 1, 2, 4, 3];
+
+        // Act
+        var result = TapeEquilibrium.Solution(A);
+
+        // Assert
+        result.Should().Be(1);
+    }
+
+    [Fact]
+    public void Solution_BasicCase2_ReturnsCorrect()
+    {
+        // Arrange
+        int[] A = [3, 1, 2, 4, 3];
+
+        // Act
+        var result = TapeEquilibrium.Solution(A);
+
+        // Assert
+        result.Should().Be(1);
+    }
+
+    [Fact]
+    public void Solution_GeneralCase_ReturnsCorrect()
+    {
+        // Arrange
+        int[] A = [3, 1, 2, 4, 3];
+
+        // Act
+        var result = TapeEquilibrium.Solution(A);
+
+        // Assert
+        result.Should().Be(1);
+    }
+
+    [Fact]
+    public void Solution_NegativeNumbers_ReturnsCorrect()
+    {
+        // Arrange
+        int[] A = [-10, -5, -3, -4, -1];
+
+        // Act
+        var result = TapeEquilibrium.Solution(A);
+
+        // Assert
+        result.Should().Be(3);
+    }
+
+    [Fact]
+    public void Solution_LargeNumbers_ReturnsCorrect()
+    {
+        // Arrange
+        int[] A = [1000, 10000, 100000, 1000000];
+
+        // Act
+        var result = TapeEquilibrium.Solution(A);
+
+        // Assert
+        result.Should().Be(809000);
+    }
+}
